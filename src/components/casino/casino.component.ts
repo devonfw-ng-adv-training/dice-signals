e1d686a424f8c628a2ffbfafc8bd82d984ea805d
Denis Pokojski <denis.pokojski@outlook.com>
2025-07-14 14:01:27 +0200

    Typo

--- src/components/casino/casino.component.ts
+++ src/components/casino/casino.component.ts
@@ -1,20 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CubeComponent } from '../cube/cube.component';
-import {
-  computeChance,
-  computeFourOfAKind,
-  computeFullHouse,
-  computeJackpot,
-  computeLargeStraight,
-  computePointsOfOneKind,
-  computeSmallStraight,
-  computeThreeOfAKind,
-} from '../../utils/dice.util';
 
 export type DiceResult = {
   diceNumber: number | undefined;
@@ -39,12 +24,12 @@ export class CasinoComponent {
 
   // Todo Use signals to compute the result table here using the values of the cube signals above.
 
-  /* ones = 
-     twos = 
+  /* ones =
+     twos =
   */
 
   // You can use the dice.util.ts utility to calculate the actual results for the categories (Ones, Twos, Full House, etc..)
-  // or try to write yout own computation.
+  // or try to write your own computation.
 
   // Display the computed values in the table that is prepared in the template.
 
