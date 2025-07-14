98a179de7c9d2a3c4c0484ad3fe3fe1eb2d41256
Denis Pokojski <denis.pokojski@outlook.com>
2025-07-14 14:21:45 +0200

    Implement exercise 02 rxjs

--- src/components/casino/casino.component.ts
+++ src/components/casino/casino.component.ts
@@ -1,18 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
-import { BehaviorSubject, combineLatest, map, Observable, of } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { CubeComponent } from '../cube/cube.component';
-import {
-  computeChance,
-  computeFourOfAKind,
-  computeFullHouse,
-  computeJackpot,
-  computeLargeStraight,
-  computePointsOfOneKind,
-  computeSmallStraight,
-  computeThreeOfAKind,
-} from '../../utils/dice.util';
 
 export type CubeResult = {
   cubeNumber: number | undefined;
@@ -35,64 +25,12 @@ export class CasinoComponent implements OnInit {
   dice4$ = new BehaviorSubject<number>(6);
   dice5$ = new BehaviorSubject<number>(6);
 
-  ones$: Observable<number> | undefined;
-  twos$: Observable<number> | undefined;
-  threes$: Observable<number> | undefined;
-  fours$: Observable<number> | undefined;
-  fives$: Observable<number> | undefined;
-  sixes$: Observable<number> | undefined;
-  threeOfAKind$: Observable<number> | undefined;
-  fourOfAKind$: Observable<number> | undefined;
-  fullHouse$: Observable<number> | undefined;
-  smallStraight$: Observable<number> | undefined;
-  largeStraight$: Observable<number> | undefined;
-  jackpot$: Observable<number> | undefined;
-  chance$: Observable<number> | undefined;
+  // Todo Define observables here to handle categories "ones", "twos",...
 
   ngOnInit() {
-    const alldDice$ = combineLatest([
-      of(0),
-      this.dice1$,
-      this.dice2$,
-      this.dice3$,
-      this.dice4$,
-      this.dice5$,
-    ]);
-
-    this.ones$ = alldDice$.pipe(
-      map((points) => computePointsOfOneKind(points, 1)),
-    );
-    this.twos$ = alldDice$.pipe(
-      map((points) => computePointsOfOneKind(points, 2)),
-    );
-    this.threes$ = alldDice$.pipe(
-      map((points) => computePointsOfOneKind(points, 3)),
-    );
-    this.fours$ = alldDice$.pipe(
-      map((points) => computePointsOfOneKind(points, 4)),
-    );
-    this.fives$ = alldDice$.pipe(
-      map((points) => computePointsOfOneKind(points, 5)),
-    );
-    this.sixes$ = alldDice$.pipe(
-      map((points) => computePointsOfOneKind(points, 6)),
-    );
-
-    this.threeOfAKind$ = alldDice$.pipe(
-      map((points) => computeThreeOfAKind(points)),
-    );
-    this.fourOfAKind$ = alldDice$.pipe(
-      map((points) => computeFourOfAKind(points)),
-    );
-    this.fullHouse$ = alldDice$.pipe(map((points) => computeFullHouse(points)));
-    this.smallStraight$ = alldDice$.pipe(
-      map((points) => computeSmallStraight(points)),
-    );
-    this.largeStraight$ = alldDice$.pipe(
-      map((points) => computeLargeStraight(points)),
-    );
-    this.jackpot$ = alldDice$.pipe(map((points) => computeJackpot(points)));
-    this.chance$ = alldDice$.pipe(map((points) => computeChance(points)));
+    // Todo Set the calculations for the above defined observables here
+    // You can use the dice.util.ts utility to calculate the actual results for the categories
+    // or try to write your own computation.
   }
 
   onCubeChange(result: CubeResult | null) {
